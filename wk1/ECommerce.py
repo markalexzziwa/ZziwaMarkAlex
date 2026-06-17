678c91497a918134cd1f1294d5e53ed89a0d7a3d
Zziwa Mark Alex <zziwamarkalex21@gmail.com>
2026-06-17 09:54:34 +0300

    ecomm edit

--- wk1/ECommerce.py
+++ wk1/ECommerce.py
@@ -12,6 +12,20 @@ locationdiscounts = {
 }
 if username == "admin" and password == "adminpwd123":
     print("Login successful! Welcome, admin.")
+elif username == "cashier" and password == "cashpwd123":
+    print("Login successful! Welcome, cashier.")
+    total_amount = float(input("Please enter the total amount of your purchase: "))
+    print("Login successful! Welcome, customer.")
+else:
+    print("Login failed! Invalid username or password.")
+
+if username in ["admin", "customer"]:
+    print("Here are the available products:")
+    for product in products:
+        print("-> " + product)
+    print("supported locations: ", ", ".join(locations))
+
+if username in ["admin", "cashier"]:
     total_amount = float(input("Please enter the total amount of your purchase: "))
     shipping_cost = int(input("Please enter the shipping cost: "))
     coupon_code = input("Please enter your coupon code: ")
@@ -44,38 +58,4 @@ if username == "admin" and password == "adminpwd123":
             print(f"Your total cost including shipping is: UGX.{total_cost:.2f}")
         case _:
             print("Invalid coupon code. No discounts applied.")
-elif username == "cashier" and password == "cashpwd123":
-    print("Login successful! Welcome, cashier.")
-    total_amount = float(input("Please enter the total amount of your purchase: "))
-    coupon_code = input("Please enter your coupon code: ")
-    shipping_cost = int(input("Please enter the shipping cost: "))
-    match coupon_code:
-        case "d20t10":
-            print("You have received a 20% discount on your purchase!")
-            discount_amount = total_amount * 0.20
-            total_amount -= discount_amount
-            tax = total_amount * 0.10
-            total_amount += tax
-            print(f"Total amount after discount and tax is: UGX.{total_amount:.2f}")
-            total_cost = total_amount + shipping_cost
-            location = input("Please enter your location: ").lower()
-            if location in locationdiscounts:
-                location_discount = locationdiscounts[location]
-                print(f"You have received an additional {location_discount*100:.0f}% discount based on your location!")
-                total_amount -= total_amount * location_discount
-            print(f"Your total cost including shipping is: UGX.{total_cost:.2f}")
-        case "F00t20":
-            print("You have received free shipping on your order!")
-            tax = total_amount * 0.20
-            total_amount += tax
-            total_cost = total_amount
-            print(f"Your total cost including shipping is: UGX.{total_cost:.2f}")
-        case _:
-            print("Invalid coupon code. No discounts applied.")
-elif username == "customer" and password == "cuspwd123":
-    print("Login successful! Welcome, customer.")
-    print("Here are the available products:")
-    for product in products:
-        print("- " + product)
-else:
-    print("Login failed! Invalid username or password.")
+        
---
+++ wk2/Dataframe.py
@@ -0,0 +1,8 @@
+import pandas
+data = {
+    "Name": ["John", "Alice", "Bob"],
+    "Age": [25, 30, 22],
+}
+print(data)
+df = pandas.DataFrame(data)
+print(df)
